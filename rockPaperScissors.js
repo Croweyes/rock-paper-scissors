f3bed17324ec4e9125b2116b3d472b6356d7f111
Croweyes <219825403+Croweyes@users.noreply.github.com>
2026-04-16 02:20:05 -0600

    Add step 3 of the game The computer will give 3 options for the player to reply with

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!")
+console.log("Lets play rock, scissors, paper!")
+function getComputerChoice() {
+    let pick
+    let numberChoice = Math.floor(Math.random() * 3)
+    if (numberChoice == 0) {
+        pick = "Scissors"
+        console.log(pick)
+    }
+    else if (numberChoice == 1) {
+        pick = "Paper"
+        console.log(pick)
+    } 
+    else {
+        pick = "Rock"
+        console.log(pick)
+    }
+}
+
+getComputerChoice()
