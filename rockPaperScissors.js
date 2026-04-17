55d1e3c61ec953bc3aa3b4626274adf00d77cf95
Croweyes <219825403+Croweyes@users.noreply.github.com>
2026-04-16 23:05:22 -0600

    Add partial completion of the step 5 of the project

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,20 +1,23 @@
 let humanScore = 0
 let computerScore = 0
+const humanChoice = getHumanChoice()
+const computerChoice = getComputerChoice()
 
 console.log("Lets play rock, scissors, paper!")
+
 function getComputerChoice() {
     let pick
     let numberChoice = Math.floor(Math.random() * 3)
     if (numberChoice == 0) {
-        pick = "Scissors"
+        pick = "scissors"
         console.log(pick)
     }
     else if (numberChoice == 1) {
-        pick = "Paper"
+        pick = "paper"
         console.log(pick)
     } 
     else {
-        pick = "Rock"
+        pick = "rock"
         console.log(pick)
     }
 }
@@ -24,5 +27,53 @@ function getHumanChoice() {
     console.log(pick)
 }
 
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice.toLowerCase())
+
+    if (humanChoice == "rock") {
+        humanChoice = "B"
+        if (computerChoice == "scissors") {
+            computerChoice = "C"
+        }
+        else {
+            computerChoice = "A"
+        }
+    }
+
+    else if (humanChoice == "scissors") {
+        humanChoice = "B"
+        if (computerChoice == "paper") {
+            computerChoice = "C"
+        }
+        else {
+            computerChoice = "A"
+        }
+    }
+
+    else {
+        humanChoice = "B"
+        if (computerChoice == "rock") {
+            computerChoice = "C"
+        }
+        else {
+            computerChoice = "A"
+        }
+    }
+
+    if (humanChoice > computerChoice) {
+        humanScore += 1
+    }
+
+    else if (computerChoice > humanChoice) {
+        computerScore += 1
+    }
+    else {
+        console.log("Tie")
+    }
+}
+
 getComputerChoice()
 getHumanChoice()
+playRound()
+console.log("You: " + humanScore)
+console.log("Computer: " + computerScore)
